3a275cdb3a57a7696c59f114558dcb7d3e7edd29
Justin Musgrove <justin.musgrove@gmail.com>
2013-09-13 10:55:47 -0500

    fix arch name, move todos out of project to private repo

--- src/test/java/com/levelup/java/Permutations.java
+++ src/test/java/com/levelup/java/Permutations.java
@@ -11,6 +11,17 @@ import com.google.common.collect.Lists;
 
 public class Permutations {
 
+//	https://www.khanacademy.org/math/probability/probability-and-combinatorics-topic/permutations_and_combinations/v/permutations	
+	
+//"My fruit salad is a combination of apples, grapes and bananas" We don't care what order the fruits are in, they could also be "bananas, grapes and apples" or "grapes, apples and bananas", its the same fruit salad.
+//	 
+//"The combination to the safe was 472". Now we do care about the order. "724" would not work, nor would "247". It has to be exactly 4-7-2.
+//So, in Mathematics we use more precise language:
+//
+//If the order doesn't matter, it is a Combination.
+//If the order does matter it is a Permutation.	
+	
+	
 	@Test
 	public void test() {
 
@@ -24,7 +35,6 @@ public class Permutations {
 
 		List<String> elements3 = Lists.newArrayList("b", "c", "a");
 		System.out.println(Iterables.concat(elements, elements3));
-
 		
 	}
 
--- src/test/java/com/levelup/java/collections/SortACollection.java
+++ src/test/java/com/levelup/java/collections/SortACollection.java
@@ -1,51 +1,197 @@
 package com.levelup.java.collections;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.math.BigDecimal;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import com.google.common.base.Objects;
+import com.google.common.collect.Collections2;
+import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Ordering;
+import com.google.common.primitives.Doubles;
 import com.google.common.primitives.Ints;
 
 public class SortACollection {
 
 //	In this exercise we are going to sort a list of transaction by
-//	transactions amount with 0 armount firsthave a list of transactions
-//  that will be sorted.  While a collection maintains order
+//	transactions decending.  While a collection maintains order
 //	it may be important sort on a web page or process nulls first.
-//	
+
+// keywords: sort a collection in java, how to order a collection list in java	
+//	http://www.polygenelubricants.com/2010/10/elegant-comparison-logic-with-guava.html
 	
+	class Transaction {
+		
+		private BigDecimal id;
+		private double amount;
+		
+		public Transaction(BigDecimal id, double amount) {
+			super();
+			this.id = id;
+			this.amount = amount;
+		}
+
+		public BigDecimal getId() {
+			return id;
+		}
+		public double getAmount() {
+			return amount;
+		}
+		
+		@Override
+		public String toString() {
+			return Objects.toStringHelper(this).add("id", id).add("amount", amount).toString();
+		}
+
+	}
+
+	private List<Transaction> checkBook = Lists.newArrayList();
 	
+	@Before 
+	public void setup () {
+		
+		checkBook.add(new Transaction(new BigDecimal(1), 500));
+		checkBook.add(new Transaction(new BigDecimal(2), 25));
+		checkBook.add(new Transaction(new BigDecimal(3), 78));
+		checkBook.add(new Transaction(new BigDecimal(4), 222));
+		checkBook.add(new Transaction(new BigDecimal(5), 903));
+		checkBook.add(new Transaction(new BigDecimal(6), 0));
+		
+	}
 	
 	@Test
 	public void sort_collection_with_java () {
-//		Collections.sort(l);
+		
+		Comparator<Transaction> byAmount = new Comparator<Transaction>() {
+		    public int compare(Transaction left, Transaction right) {
+		        return Double.compare(left.getAmount(), right.getAmount()) ; // use your logic
+		    }
+		};
+		
+//		1. figure out how to chain comparators with standard java
+//		Lists
+//		Collections.reverseOrder() byAmount
+
+//		Collections.sort(checkBook, ); // use the comparator as much as u want
+
+		//[Transaction{id=903.0}, Transaction{id=500.0}, Transaction{id=222.0}, Transaction{id=78.0}, Transaction{id=25.0}]
+
+		System.out.println(checkBook);
 	}
 	
+//	The ordering class is an enhanced comparator, with additional methods to support common operations. 
+//	If are familiar with the FluentIterable, FluentIterable is to Iterable as Ordering is to comparator.
+	
+	Ordering<Transaction> byAmount = new Ordering<Transaction>() {
+	  public int compare(Transaction left, Transaction right) {
+	    return Doubles.compare(left.getAmount(), right.getAmount());
+	  }
+	};
+	
+	// TODO determine right way to compare BigDecimal
+	
+	Ordering<Transaction> byId = new Ordering<Transaction>() {
+	  public int compare(Transaction left, Transaction right) {
+	    return Doubles.compare(left.getId().intValue(), right.getId().intValue());
+	  }
+	};
+	
+	@Test
+	public void sort_collection_with_guava () {
+		
+		List<Transaction> highestTransactions = byAmount.greatestOf(checkBook, 3);
+		
+		//[Transaction{id=903.0}, Transaction{id=500.0}, Transaction{id=222.0}]
+		
+		Transaction highestTransaction = Iterables.getFirst(highestTransactions, null);
+
+		assertNotNull(highestTransaction);
+		assertEquals(903, highestTransaction.getAmount(), 0);
+	}
+	
+	@Test
+	public void sort_collection_with_guava_least () {
+		
+		List<Transaction> lowestTransactions = byAmount.leastOf(checkBook, 3);
+
+//		System.out.println(lowestTransactions);
+		
+		// lowestTransactions = [Transaction{id=6, amount=0.0}, Transaction{id=2, amount=25.0}, Transaction{id=3, amount=78.0}]
+
+		Transaction lowestTransaction = Iterables.getFirst(lowestTransactions, null);
+		
+		assertNotNull(lowestTransactions);
+		assertEquals(0, lowestTransaction.getAmount(), 0);
+		
+	}
+	
+	// In the instance you want to chain multiple iterators together you can easily do this with guava
+	// lets validate that the ordering of ids w/ comparables
+	
+	@Test
+	public void sort_collection_with_guava_byId () {
+		List<Transaction> lowestTransactionById = byId.leastOf(checkBook, 1);
+
+		// lowestTransactionById = [Transaction{id=1, amount=500.0}]
+
+		Transaction lowestTransaction = Iterables.getFirst(lowestTransactionById, null);
+		
+		assertNotNull(lowestTransactionById);
+		assertEquals(new BigDecimal(1), lowestTransaction.getId());
+	}
 	
 	
 	@Test
-	public void sortget_last_element_in_list_with_guava () {
+	public void chaining_multiple_comparables_with_guava () {
 		
-		Ordering<String> byLengthOrdering = new Ordering<String>() {
-		  public int compare(String left, String right) {
-		    return Ints.compare(left.length(), right.length());
-		  }
-		};
+		// the byAmount comparator is set up to order ascending
+		// 		[Transaction{id=6, amount=0.0}, Transaction{id=2, amount=25.0}, Transaction{id=3, amount=78.0}, Transaction{id=4, amount=222.0}, Transaction{id=1, amount=500.0}, Transaction{id=5, amount=903.0}]
+		//		[Transaction{id=6, amount=0.0}, Transaction{id=2, amount=25.0}, Transaction{id=3, amount=78.0}, Transaction{id=4, amount=222.0}, Transaction{id=1, amount=500.0}, Transaction{id=5, amount=903.0}]
 
-		List<String> something = Lists.newArrayList("a", "ab", "abc", "abcd", "abcde", null, null);
+		// if we compund muliple comparables (byAmount and byId)
+		// we should get the lowest amount and id
+
+
+		// byid
+		//		[Transaction{id=1, amount=500.0}, Transaction{id=2, amount=25.0}, Transaction{id=3, amount=78.0}, Transaction{id=4, amount=222.0}, Transaction{id=5, amount=903.0}, Transaction{id=6, amount=0.0}]
+		//		[Transaction{id=1, amount=500.0}, Transaction{id=2, amount=25.0}, Transaction{id=3, amount=78.0}, Transaction{id=4, amount=222.0}, Transaction{id=5, amount=903.0}, Transaction{id=6, amount=0.0}]
+
+//		[Transaction{id=6, amount=0.0}, Transaction{id=2, amount=25.0}, Transaction{id=3, amount=78.0}, Transaction{id=4, amount=222.0}, Transaction{id=1, amount=500.0}, Transaction{id=5, amount=903.0}]
+
+//		[Transaction{id=5, amount=903.0}, Transaction{id=1, amount=500.0}, Transaction{id=4, amount=222.0}, Transaction{id=3, amount=78.0}, Transaction{id=2, amount=25.0}, Transaction{id=6, amount=0.0}]
+
+//		Collections.sort(checkBook, byAmount	
+//										.compound(
+//												byId.reverse())
+//										.reverse());
+//		System.out.println(checkBook);
 		
-		System.out.println(byLengthOrdering.nullsFirst().reverse());;
+//		System.out.println(byAmount.greatestOf(checkBook, 10));
+//		System.out.println(byAmount.compound(byId.greatestOf(checkBook, k)).greatestOf(checkBook, 10));
 		
-//		byLengthOrdering.
-		Collections.sort(something, byLengthOrdering.nullsFirst().reverse() );
+//		Ordering.compound()
 		
-		System.out.println(something);
+//		System.out.println(checkBook);
 		
+		// by default our comparator will byAmount
+//		byAmount.compound(byId);
+		
+//		List<Transaction> orderedByAmountAndId = byAmount.com(checkBook, 3);
+
 		
 	}
+	
+//	@Test 
+//	public void  
+	
 		
 	
 }
