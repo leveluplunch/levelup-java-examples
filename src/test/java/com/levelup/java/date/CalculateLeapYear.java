17d4b73700e6316960aa94d75eabc90739e51d20
Justin Musgrove <justin.musgrove@gmail.com>
2013-09-18 19:25:24 -0500

    added date examples

---
+++ src/test/java/com/levelup/java/date/CalculateLeapYear.java
@@ -0,0 +1,48 @@
+package com.levelup.java.date;
+
+import static org.junit.Assert.assertFalse;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import org.joda.time.DateTime;
+import org.junit.Test;
+
+/**
+ * This java snippet will demonstrates how determine leap year in java.
+ * 
+ * @author Justin Musgrove
+ * @see <a href='http://www.leveluplunch.com/java/'></a>
+ * 
+ */
+public class CalculateLeapYear {
+
+	@Test
+	public void calculate_leap_year_in_java_with_calendar () {
+
+		  Calendar cal = Calendar.getInstance();
+		  cal.set(Calendar.YEAR, 2007);
+		  
+		  assertFalse(cal.getActualMaximum(Calendar.DAY_OF_YEAR) > 365);
+
+	}
+	
+	@Test
+	public void calculate_leap_year_in_java_with_gregorian_calendar () {
+
+		GregorianCalendar cal = new GregorianCalendar();  
+		assertFalse(cal.isLeapYear(2007));
+
+	}
+
+	@Test
+	public void calculate_leap_year_in_java_with_yoda () {
+		
+		DateTime dateTime = new DateTime();
+		boolean isLeapYear = dateTime.withYear(2007).year().isLeap();
+		
+		assertFalse(isLeapYear);
+	}
+
+	
+}
---
+++ src/test/java/com/levelup/java/date/DateToXMLGregorianCalendar.java
@@ -0,0 +1,34 @@
+package com.levelup.java.date;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.util.GregorianCalendar;
+
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
+
+import org.junit.Test;
+
+/**
+ * This java snippet will demonstrate converting a date to XMLGregorianCalendar
+ * 
+ * @author Justin Musgrove
+ * @see <a href='http://www.leveluplunch.com/java/'></a>
+ * 
+ */
+public class DateToXMLGregorianCalendar {
+
+	
+	@Test
+	public void convert_date_to_XMLGregorianCalendar () throws DatatypeConfigurationException { 
+
+		GregorianCalendar gCalendar = new GregorianCalendar();
+
+		XMLGregorianCalendar xmlGregorianCalendar = DatatypeFactory.newInstance().newXMLGregorianCalendar(gCalendar);
+		
+		assertNotNull(xmlGregorianCalendar);
+
+	}
+	
+}
