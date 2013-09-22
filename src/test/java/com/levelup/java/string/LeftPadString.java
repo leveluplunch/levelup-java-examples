2367805acc050e72243d3728f23c5d9a95f65e0c
Justin Musgrove <justin.musgrove@gmail.com>
2013-09-21 22:13:47 -0500

    added right and left pad examples

---
+++ src/test/java/com/levelup/java/string/LeftPadString.java
@@ -0,0 +1,74 @@
+package com.levelup.java.string;
+
+import static org.hamcrest.CoreMatchers.startsWith;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Test;
+
+import com.google.common.base.Strings;
+
+/**
+ * This java snippet will demonstrate left padding a string
+ * of size 10 with spaces and zeros.
+ * 
+ * @author Justin Musgrove
+ * @see <a href='http://www.leveluplunch.com/java/'></a>
+ * 
+ */
+public class LeftPadString {
+
+	@Test
+	public void left_pad_string_with_zeros_java () {
+
+		String leftPaddedString = String.format("%10s", "levelup").replace(' ', '0');
+
+		assertEquals(10, leftPaddedString.length());
+		assertThat(leftPaddedString, startsWith("0"));
+	}
+	
+	@Test
+	public void left_pad_string_with_spaces_java () {
+
+		String leftPaddedString = String.format("%10s", "levelup").replace(' ', ' ');
+
+		assertEquals(10, leftPaddedString.length());
+		assertThat(leftPaddedString, startsWith(" "));
+	}
+
+	@Test
+	public void left_pad_string_with_zeros_guava () {
+		
+		String leftPaddedString = Strings.padStart("levelup", 10, '0');
+		
+		assertEquals(10, leftPaddedString.length());
+		assertThat(leftPaddedString, startsWith("0"));
+	}
+
+	@Test
+	public void left_pad_string_with_spaces_guava () {
+
+		String leftPaddedString = Strings.padStart("levelup", 10, ' ');
+		
+		assertEquals(10, leftPaddedString.length());
+		assertThat(leftPaddedString, startsWith(" "));
+	}
+	
+	@Test
+	public void left_pad_string_with_zeros_apache_commons () {
+		String leftPaddedString = StringUtils.leftPad("levelup", 10, "0");
+		
+		assertEquals(10, leftPaddedString.length());
+		assertThat(leftPaddedString, startsWith("0"));
+	}
+
+	@Test
+	public void left_pad_string_with_spaces_apache_commons () {
+		String leftPaddedString = StringUtils.leftPad("levelup", 10, " ");
+		
+		assertEquals(10, leftPaddedString.length());
+		assertThat(leftPaddedString, startsWith(" "));
+	}
+	
+}
---
+++ src/test/java/com/levelup/java/string/RightPadString.java
@@ -0,0 +1,76 @@
+package com.levelup.java.string;
+
+import static org.hamcrest.CoreMatchers.endsWith;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Test;
+
+import com.google.common.base.Strings;
+
+/**
+ * This java snippet will demonstrate right padding a string
+ * of size 10 with spaces and 0
+ * 
+ * @author Justin Musgrove
+ * @see <a href='http://www.leveluplunch.com/java/'></a>
+ * 
+ */
+public class RightPadString {
+
+	@Test
+	public void right_pad_string_with_zeros_java () {
+
+		String rightPaddedString = String.format("%-10s", "levelup").replace(' ', '0');
+
+		assertEquals(10, rightPaddedString.length());
+		assertThat(rightPaddedString, endsWith("0"));
+	}
+	
+	@Test
+	public void right_pad_string_with_spaces_java () {
+
+		String rightPaddedString = String.format("%-10s", "levelup").replace(' ', ' ');
+
+		assertEquals(10, rightPaddedString.length());
+		assertThat(rightPaddedString, endsWith(" "));
+	}
+
+	@Test
+	public void right_pad_string_with_zeros_guava () {
+		
+		String rightPaddedString = Strings.padEnd("levelup", 10, '0');
+		
+		assertEquals(10, rightPaddedString.length());
+		assertThat(rightPaddedString, endsWith("0"));
+	}
+
+	@Test
+	public void right_pad_string_with_spaces_guava () {
+
+		String rightPaddedString = Strings.padEnd("levelup", 10, ' ');
+		
+		assertEquals(10, rightPaddedString.length());
+		assertThat(rightPaddedString, endsWith(" "));
+	}
+	
+	@Test
+	public void right_pad_string_with_zeros_apache_commons () {
+		String rightPaddedString = StringUtils.rightPad("levelup", 10, "0");
+		
+		assertEquals(10, rightPaddedString.length());
+		assertThat(rightPaddedString, endsWith("0"));
+	}
+
+	@Test
+	public void right_pad_string_with_spaces_apache_commons () {
+		String rightPaddedString = StringUtils.rightPad("levelup", 10, " ");
+		
+		assertEquals(10, rightPaddedString.length());
+		assertThat(rightPaddedString, endsWith(" "));
+	}
+
+	
+	
+}
