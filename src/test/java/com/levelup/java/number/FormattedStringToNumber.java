0ef2efdb1a2862aa7a2aa0599a75ed947025df6a
Justin Musgrove <justin.musgrove@gmail.com>
2013-09-28 11:18:09 -0500

    add formatted string to number

---
+++ src/test/java/com/levelup/java/number/FormattedStringToNumber.java
@@ -0,0 +1,29 @@
+package com.levelup.java.number;
+
+import static org.junit.Assert.assertEquals;
+
+import java.text.DecimalFormat;
+import java.text.ParseException;
+
+import org.junit.Test;
+
+/**
+ * This java snippet will demonstrate converting
+ * a formatted string to a number
+ * 
+ * @author Justin Musgrove
+ * @see <a href='http://www.leveluplunch.com/java/'></a>
+ * 
+ */
+public class FormattedStringToNumber {
+	
+	@Test
+	public void convert_formatted_string_to_number_java () throws ParseException {
+		DecimalFormat df = new DecimalFormat("#,##0.00;(#,##0.00)");
+		
+		Number parsedNumber = df.parse("123,234.25");
+		
+		assertEquals(123234.25, parsedNumber);
+	}
+	
+}
