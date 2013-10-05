ec31caa7472da066cc3d70eeace735fd4cebd9e0
Justin Musgrove <justin.musgrove@gmail.com>
2013-10-05 09:23:08 -0500

    change method names, added bigdecimal is close to

--- src/test/java/com/levelup/java/hamcrest/NumberMatchers.java
+++ src/test/java/com/levelup/java/hamcrest/NumberMatchers.java
@@ -5,7 +5,9 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.number.IsCloseTo.*;
 import static org.hamcrest.number.OrderingComparison.*;
 import static org.junit.Assert.assertThat;
+import static org.hamcrest.number.BigDecimalCloseTo.*;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import org.junit.Test;
@@ -23,31 +25,31 @@ import com.google.common.collect.Lists;
 public class NumberMatchers {
 
 	@Test
-	public void item_close_to () {
+	public void number_close_to_number () {
 		assertThat(200.24, is(closeTo(200, 1)));
 	}
 	
 	@Test
-	public void item_greaterthan () {
+	public void number_greaterthan () {
 		assertThat(20, greaterThan(18));
 	}
 	
 	@Test
-	public void everyitem_in_list_greaterthan () {
+	public void everyitem_in_list_greaterthan_number () {
 		 
 		List<Integer> ages = Lists.newArrayList(21, 25, 30);
 		assertThat(ages, everyItem(greaterThan(18)));
 	}
 	
 	@Test
-	public void everyitem_in_list_greaterthan_or_equal_to () {
+	public void everyitem_in_list_greaterthan_or_equal_to_number () {
 		 
 		List<Integer> ages = Lists.newArrayList(21, 25, 30, 18);
 		assertThat(ages, everyItem(greaterThanOrEqualTo(18)));
 	}
 
 	@Test
-	public void everyitem_in_list_lessthan () {
+	public void everyitem_in_list_lessthan_number() {
 		 
 		List<Integer> ages = Lists.newArrayList(21, 25, 30);
 		assertThat(ages, everyItem(lessThan(31)));
@@ -55,11 +57,21 @@ public class NumberMatchers {
 
 	
 	@Test
-	public void everyitem_in_list_lessthan_or_equal_to () {
+	public void everyitem_in_list_lessthan_or_equal_to_number () {
 		 
 		List<Integer> ages = Lists.newArrayList(21, 25, 30, 18);
 		assertThat(ages, everyItem(lessThanOrEqualTo(30)));
 	}
 
+	@Test
+	public void bigdecimal_is_close_to_bigdecimal () {
+		
+		BigDecimal seniorCitizen = new BigDecimal(65);
+		
+		// is close to retirement 
+		assertThat(new BigDecimal(60), is(closeTo(seniorCitizen, new BigDecimal(5))));
+	
+	}
+	
 	
 }
