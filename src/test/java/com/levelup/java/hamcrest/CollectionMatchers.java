779f44a92d2d071d3e1fc192e5ec4557465aaa5c
Justin Musgrove <justin.musgrove@gmail.com>
2013-10-05 09:41:32 -0500

    added iterable test cases

--- src/test/java/com/levelup/java/hamcrest/CollectionMatchers.java
+++ src/test/java/com/levelup/java/hamcrest/CollectionMatchers.java
@@ -23,6 +23,9 @@ import static org.junit.Assert.assertThat;
 import java.util.List;
 import java.util.Map;
 
+import org.hamcrest.collection.IsIterableContainingInAnyOrder;
+import org.hamcrest.collection.IsIterableContainingInOrder;
+import org.hamcrest.collection.IsIterableWithSize;
 import org.junit.Test;
 
 import com.google.common.collect.Lists;
@@ -37,6 +40,38 @@ import com.google.common.collect.Maps;
  * 
  */
 public class CollectionMatchers {
+	
+	// iterables
+	@Test
+	public void check_size_of_iterable () {
+		
+		List<String> cloths = Lists.newArrayList(
+				"shirts", "shoes", "pants", "socks");
+		
+		assertThat(cloths, IsIterableWithSize.<String>iterableWithSize(4));
+	}
+	
+	@Test 
+	public void iterable_has_any_order () {
+
+		List<String> cloths = Lists.newArrayList(
+				"shirts", "shoes", "pants", "socks");
+		
+		assertThat(cloths, IsIterableContainingInAnyOrder.
+				<String>containsInAnyOrder("shoes", "pants", "shirts", "socks"));
+		
+	}
+	
+	@Test
+	public void iterable_matches_order () {
+
+		List<String> cloths = Lists.newArrayList(
+				"shirts", "shoes", "pants", "socks");
+		
+		assertThat(cloths, IsIterableContainingInOrder.
+				<String>contains("shirts", "shoes", "pants", "socks"));
+
+	}
 	
 //	array
 	@Test
