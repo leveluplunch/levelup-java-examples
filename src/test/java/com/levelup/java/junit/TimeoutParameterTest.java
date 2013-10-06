4abcff5a8c8b6ce0e278489049d65cb9b827fa21
Justin Musgrove <justin.musgrove@gmail.com>
2013-10-05 22:31:29 -0500

    add TimeoutParameterTest

---
+++ src/test/java/com/levelup/java/junit/TimeoutParameterTest.java
@@ -0,0 +1,27 @@
+package com.levelup.java.junit;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * This java example will demonstrate how to limit
+ * the execution time on a junit test with a timeout parameters.
+ * This is helpful if you mix integration test with unit test or if 
+ * you have long running tests.
+ * 
+ * @author Justin Musgrove
+ * @see <a href='http://www.leveluplunch.com/java/example/junit-limit-length-of-test/'>Limit length of test</a>
+ * 
+ */
+public class TimeoutParameterTest {
+
+	@Ignore("Test will always fail")
+	@Test(timeout=100)
+	public void limit_test_execution_time () {
+
+		//Infinite loop
+		while (true) {
+		}
+	}
+	
+}
