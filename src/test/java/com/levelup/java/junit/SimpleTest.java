75efb78d2fa86dfe26b587eed06288cfd3e401d5
Justin Musgrove <justin.musgrove@gmail.com>
2013-10-05 20:10:08 -0500

    add junit simple test

---
+++ src/test/java/com/levelup/java/junit/SimpleTest.java
@@ -0,0 +1,31 @@
+package com.levelup.java.junit;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * This java example will demonstrate a simple
+ * test using Junit
+ * 
+ * @author Justin Musgrove
+ * @see <a href='http://www.leveluplunch.com/java/examples/junit-test/'>Simple Test Junit</a>
+ * 
+ */
+public class SimpleTest {
+
+	static class Calculator {
+
+		public static int add (int a, int b) {
+			return a + b;
+		}
+		
+	}
+	
+	@Test
+	public void calculator_add () {
+		int calculatedValue = Calculator.add(5,  10);
+		assertEquals(15, calculatedValue);
+	}
+	
+}
