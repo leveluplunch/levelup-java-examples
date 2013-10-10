b9ec143ea360bace04cf106a42acaf57e119cfb9
Justin Musgrove <justin.musgrove@gmail.com>
2013-10-09 21:22:29 -0500

    add convert json to xml example

---
+++ src/test/java/com/levelup/java/xml/ConvertJSONtoXML.java
@@ -0,0 +1,64 @@
+package com.levelup.java.xml;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.junit.Test;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.google.common.base.Objects;
+
+/**
+ * This java example will demonstrate converting json
+ * to XML.  There are quite a few various libs that appear
+ * to do it, this example will focus on jackson.
+ * 
+ * @author Justin Musgrove
+ * @see <a href='http://www.leveluplunch.com/java/examples/convert-json-to-xml/'>Convert json to xml</a>
+ * 
+ */
+public class ConvertJSONtoXML {
+
+	
+	class Browser {
+		private String name;
+
+		public Browser(String name) {
+			super();
+			this.name = name;
+		}
+		
+		@Override
+		public String toString() {
+			return Objects.toStringHelper(this)
+					.add("name", name)
+					.toString();
+		}
+
+		public String getName() {
+			return name;
+		}
+	}
+	
+	
+	@Test
+	public void convert_json_to_xml_jackson () throws IOException {
+		
+		String browsersAsJson ="[{\"name\":\"Chrome\"},{\"name\":\"FireFox\"},{\"name\":\"Internet Explorer\"}]\n";
+		
+		ObjectMapper jsonMapper = new ObjectMapper();
+	    
+		@SuppressWarnings("unchecked")
+		List<Browser> browsers = jsonMapper.readValue(browsersAsJson, List.class);
+
+	    XmlMapper xmlMapper = new XmlMapper();
+	    
+	    String browsersAsXml = xmlMapper.writeValueAsString(browsers);
+	    
+	    assertTrue(browsersAsXml.length() > 0);
+	}
+	
+}
