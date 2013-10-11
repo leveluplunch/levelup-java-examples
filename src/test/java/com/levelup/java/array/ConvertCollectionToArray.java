09f0c4f6f6a661cedd2d7f923e95b6deea8d0417
Justin Musgrove <justin.musgrove@gmail.com>
2013-10-10 20:01:36 -0500

    updated ULR and logger

--- src/test/java/com/levelup/java/array/ConvertCollectionToArray.java
+++ src/test/java/com/levelup/java/array/ConvertCollectionToArray.java
@@ -2,9 +2,11 @@ package com.levelup.java.array;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.apache.commons.lang3.ArrayUtils;
+import org.apache.log4j.Logger;
 import org.junit.Test;
 
 import com.google.common.collect.Lists;
@@ -14,18 +16,21 @@ import com.google.common.primitives.Doubles;
  * This java example will demonstrate converting a collection to array
  * 
  * @author Justin Musgrove
- * @see <a href='http://www.leveluplunch.com/java/'></a>
+ * @see <a href='http://www.leveluplunch.com/java/examples/convert-collection-to-array/'>Convert collection to array</a>
  * 
  */
 public class ConvertCollectionToArray {
 
-	
+	private static final Logger logger = Logger.getLogger(ConvertCollectionToArray.class);
+
 	@Test
 	public void convert_collection_of_strings_to_array_with_java () {
 
 		List<String> searchEnginesAsList = Lists.newArrayList("Google", "Bing", "Yahoo", "Ask", "AOL"); 
 		
 		String[] searchEngines = searchEnginesAsList.toArray(new String[searchEnginesAsList.size()]);
+		
+		logger.info(Arrays.toString(searchEngines));
 		
 		assertEquals(5, searchEngines.length);
 		
@@ -45,11 +50,14 @@ public class ConvertCollectionToArray {
 		for (int x=0; x <= searchEngineMarketShare.size() - 1; x ++) {
 			searchEngineMarketShareArray[x] = searchEngineMarketShare.get(x);
 		}
+		
+		logger.info(Arrays.toString(searchEngineMarketShareArray));
+
 		assertEquals(5, searchEngineMarketShareArray.length);
 	}
 
 	@Test
-	public void convert_collection_of_primitives_to_array_with_guava () {
+	public void convert_collection_of_objects_to_primitive_array_with_guava () {
 		
 		List<Double> searchEngineMarketShare = Lists.newArrayList();
 		searchEngineMarketShare.add(67.1);
@@ -60,12 +68,14 @@ public class ConvertCollectionToArray {
 
 		double[] searchEngineMarketShareArray = Doubles.toArray(searchEngineMarketShare);
 		
+		logger.info(Arrays.toString(searchEngineMarketShareArray));
+		
 		assertEquals(5, searchEngineMarketShareArray.length);
 		
 	}
 	
 	@Test
-	public void convert_collection_of_primitives_to_array_with_apachecommons () {
+	public void convert_collection_of_objects_to_array_with_apachecommons () {
 
 		List<Double> searchEngineMarketShare = Lists.newArrayList();
 		searchEngineMarketShare.add(67.1);
@@ -74,9 +84,14 @@ public class ConvertCollectionToArray {
 		searchEngineMarketShare.add(2.7);
 		searchEngineMarketShare.add(1.6);
 
-		Double[] searchEngineMarketShareDoubleArray = searchEngineMarketShare.toArray(new Double[searchEngineMarketShare.size()]);
-		double[] searchEngineMarketShareArray = ArrayUtils.toPrimitive(searchEngineMarketShareDoubleArray);
+		Double[] searchEngineMarketShareDoubleArray = searchEngineMarketShare
+				.toArray(new Double[searchEngineMarketShare.size()]);
+		
+		double[] searchEngineMarketShareArray = 
+				ArrayUtils.toPrimitive(searchEngineMarketShareDoubleArray);
 
+		logger.info(Arrays.toString(searchEngineMarketShareArray));
+		
 		assertEquals(5, searchEngineMarketShareArray.length);
 	}
 
