b5ddc1c674a2b883511d7bdc4d12b65daabfb0d3
Justin Musgrove <justin.musgrove@gmail.com>
2013-10-12 15:12:49 -0500

    format added url

--- src/test/java/com/levelup/java/string/RightTrimString.java
+++ src/test/java/com/levelup/java/string/RightTrimString.java
@@ -13,7 +13,7 @@ import com.google.common.base.CharMatcher;
  *  
  * 
  * @author Justin Musgrove
- * @see <a href='http://www.leveluplunch.com/java/examples/trim-'></a>
+ * @see <a href='http://www.leveluplunch.com/java/examples/right-trim-string/'>Right trim string</a>
  * 
  */
 public class RightTrimString {
@@ -21,28 +21,36 @@ public class RightTrimString {
 	@Test
 	public void trim_trailing_spaces_from_string_guava () {
 		
-		String rightTrimmedString = CharMatcher.WHITESPACE.trimTrailingFrom("       Something in the air       ");
+		String rightTrimmedString = CharMatcher.WHITESPACE
+				.trimTrailingFrom("       Something in the air       ");
+		
 		assertEquals("       Something in the air", rightTrimmedString);
 	}
 
 	@Test
 	public void trim_trailing_spaces_from_string_apache_commons () {
 		
-		String rightTrimmedString = StringUtils.stripEnd("           Learning to fly          ", " ");
+		String rightTrimmedString = StringUtils
+				.stripEnd("           Learning to fly          ", " ");
+		
 		assertEquals("           Learning to fly", rightTrimmedString);
 	}
 
 	@Test
 	public void trim_trailing_spaces_from_string_spring_with_trimtrailingWhiteSpace () {
 		
-		String rightTrimmedString = org.springframework.util.StringUtils.trimTrailingWhitespace("      Don't come around here no more          ");
+		String rightTrimmedString = org.springframework.util
+				.StringUtils.trimTrailingWhitespace("      Don't come around here no more          ");
+		
 		assertEquals("      Don't come around here no more", rightTrimmedString);
 	}
 	
 	@Test
 	public void trim_trailing_spaces_from_string_spring_with_trim_trailing_character () {
 		
-		String rightTrimmedString = org.springframework.util.StringUtils.trimTrailingCharacter("      The waiting        ", ' ');
+		String rightTrimmedString = org.springframework.util
+				.StringUtils.trimTrailingCharacter("      The waiting        ", ' ');
+		
 		assertEquals("      The waiting", rightTrimmedString);
 	}
 	
