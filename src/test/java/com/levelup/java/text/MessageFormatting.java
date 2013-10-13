dff2fd8eec7a3e9617cfb93c182f2e29fcfb84bd
Justin Musgrove <justin.musgrove@gmail.com>
2013-10-12 19:28:31 -0500

    format url

--- src/test/java/com/levelup/java/text/MessageFormatting.java
+++ src/test/java/com/levelup/java/text/MessageFormatting.java
@@ -14,7 +14,7 @@ import org.junit.Test;
  * This java example will demonstrate formatting messages in java
  * 
  * @author Justin Musgrove
- * @see <a href='http://www.leveluplunch.com/java/'></a>
+ * @see <a href='http://www.leveluplunch.com/java/examples/message-format/>Messageformat</a>
  * 
  */
 public class MessageFormatting {
@@ -28,8 +28,6 @@ public class MessageFormatting {
 
 		String output = value1 + value2 + value3 + value4;
 
-		// ouput = Your account balance is 2,500 on 5/15/2013
-
 		assertEquals("Your account balance was 2,500 on 5/15/2013", output);
 	}
 
@@ -39,7 +37,6 @@ public class MessageFormatting {
 		Object[] accountValues = { "2,500", "5/15/2013" };
 		String output = MessageFormat.format("Your account balance was {0} on {1}", accountValues);
 
-		// ouput = Your account balance is 2,500 on 5/15/2013
 		assertEquals("Your account balance was 2,500 on 5/15/2013", output);
 	}
 
@@ -56,14 +53,9 @@ public class MessageFormatting {
 			statementOutput.add(formatter.format(monthlyStatements));
 		}
 
-		// statement output:
-		// Your account balance was 2,500 on 5/15/2013
-		// Your account balance was 2,200 on 6/15/2013
-
 		assertTrue(statementOutput.size() == 2);
 		assertEquals("Your account balance was 2,500 on 5/15/2013", statementOutput.get(0));
 		assertEquals("Your account balance was 2,200 on 6/15/2013",statementOutput.get(1));
-
 	}
 
 	@Test
@@ -74,7 +66,8 @@ public class MessageFormatting {
 
 		Object[] statementValues = { satementBalance, statementDate.toDate() };
 
-		String output = MessageFormat.format("Your account balance was {0, number, currency} on {1, date, MM/dd/yyyy}", statementValues);
+		String output = MessageFormat.format("Your account balance was {0, number, currency} "
+				+ "on {1, date, MM/dd/yyyy}", statementValues);
 
 		assertEquals("Your account balance was $2,500.00 on  05/15/2013", output);
 	}
@@ -82,18 +75,21 @@ public class MessageFormatting {
 	@Test
 	public void messageformatter_with_choice() {
 		
-		String pattern = "Your statement on {0, date, MM/dd/yyyy} {1,choice,0#is not available|1#is {1, number, currency}}";
+		String pattern = "Your statement on {0, date, MM/dd/yyyy} "
+				+ "{1,choice,0#is not available|1#is {1, number, currency}}";
+		
 		MessageFormat statementFormat = new MessageFormat(pattern);
 		
 		DateTime statementDate1 = new DateTime(2013, 5, 15, 0, 0, 0, 0);
 
 		Object[] statement1 = {statementDate1.toDate(), 23444};
-		assertEquals("Your statement on  05/15/2013 is $23,444.00", statementFormat.format(statement1));
+		assertEquals("Your statement on  05/15/2013 is $23,444.00", 
+				statementFormat.format(statement1));
 		
 		DateTime statementDate2 = new DateTime(2013, 6, 15, 0, 0, 0, 0);
 		Object[] statement2 = {statementDate2.toDate(), 0};
-		assertEquals("Your statement on  06/15/2013 is not available", statementFormat.format(statement2));
-		
+		assertEquals("Your statement on  06/15/2013 is not available", 
+				statementFormat.format(statement2));
 	}
 	
 }
