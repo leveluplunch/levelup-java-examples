bd80b4966933ca7dabcea1073a8c0bf1f0952cf6
Justin Musgrove <justin.musgrove@gmail.com>
2013-10-12 21:07:26 -0500

    add logger, formatter, fix url

--- src/test/java/com/levelup/java/array/ShiftValuesInArray.java
+++ src/test/java/com/levelup/java/array/ShiftValuesInArray.java
@@ -4,28 +4,34 @@ import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
 
+import org.apache.log4j.Logger;
 import org.junit.Test;
 
 /**
  * This java example will demonstrate shifting the values in array
  * 
  * @author Justin Musgrove
- * @see <a href='http://www.leveluplunch.com/java/'></a>
+ * @see <a href='http://www.leveluplunch.com/java/examples/shift-elements-in-array/'>Shift elements in array</a>
  * 
  */
 public class ShiftValuesInArray {
-
-	String[] shoeTypes = {
-			"Nike", 
-			"Puma", 
-			"Converse", 
-			"Reebok", 
-			"Asics"};
 	
+	private static final Logger logger = Logger.getLogger(ShiftValuesInArray.class);
+
 	@Test
 	public void shift_values_in_array_with_java () {
+
+		String[] shoeTypes = {
+				"Nike", 
+				"Puma", 
+				"Converse", 
+				"Reebok", 
+				"Asics"};
+		
 		String[] shoeTypesMinusNike = new String[shoeTypes.length - 1];
 		System.arraycopy(shoeTypes, 1, shoeTypesMinusNike, 0, shoeTypes.length - 1);
+		
+		logger.info(Arrays.toString(shoeTypesMinusNike));
 		
 		assertTrue(Arrays.binarySearch(shoeTypesMinusNike, "Nike") < 0);
 	}
