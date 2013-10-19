8b3c11772ac99265a0820d36a2b48fe49bbe9e1c
Justin Musgrove <justin.musgrove@gmail.com>
2013-10-18 20:06:45 -0500

    update escape

--- src/test/java/com/levelup/java/net/EscapeUrl.java
+++ src/test/java/com/levelup/java/net/EscapeUrl.java
@@ -13,29 +13,24 @@ import org.springframework.web.util.UriUtils;
 import com.google.common.base.Charsets;
 import com.google.common.net.UrlEscapers;
 
+/**
+ * This java example will demonstrate encoding
+ * a URL string.
+ * 
+ * @author Justin Musgrove
+ * @see <a href='http://www.leveluplunch.com/java/examples/encode-url-string/'>Encode URL string</a>
+ * 
+ */
 public class EscapeUrl {
 	
-	// RFC2396 is a document that specifies an Internet standards track protocol for the Internet community
-	// http://www.w3.org/2002/11/dbooth-names/rfc2396-numbered_clean.htm.  There is a difference between
-	// 
-	
-//		The URLEncoder and URLDecoder classes can also be used, but only for HTML form encoding, which is not the same as the encoding scheme defined in RFC2396.
-	
-	// What is URL encoding or Percent-encoding is a mechanism for encoding information in a Uniform 
-	// Resource Identifier (URI) under certain circumstances.  For instnace, a ' ' in a URI is %20  
-	// http://en.wikipedia.org/wiki/URL_Encoding
-	
-//	http://tools.ietf.org/html/rfc3986
-	
-	// URL address encoding, how to encode url, how to encode URL to avoid special characters in java, encode URI in java, encode URL in java, how to encode URI in java
-	// Be sure to check java docs as each library handles escaping URLs similiar but some different.
 	
 	private static final String URL_TO_ESCAPE = "http://www.leveluplunch.com?somevar=abc123&someothervar";
 											   
 	@Test
 	public void escape_url_with_straight_java () throws UnsupportedEncodingException {
 
-		//The URLEncoder and URLDecoder classes can also be used, but only for HTML form encoding, which is not the same as the encoding scheme defined in RFC2396.
+		//The URLEncoder and URLDecoder classes can also be used, but only for 
+		//HTML form encoding, which is not the same as the encoding scheme defined in RFC2396.
 		// In this example we want to encode the URL not form encoding.
 
 		String urlEscaped = URLEncoder.encode(URL_TO_ESCAPE, "UTF-8")
@@ -47,7 +42,6 @@ public class EscapeUrl {
 	                .replaceAll("\\%7E", "~");	
 		
 		assertEquals("http%3A%2F%2Fwww.leveluplunch.com%3Fsomevar%3Dabc123%26someothervar", urlEscaped);
-	
 	}
 	
 	@Test
@@ -55,7 +49,6 @@ public class EscapeUrl {
 		
 		String urlEscaped = UrlEscapers.urlPathSegmentEscaper().escape(URL_TO_ESCAPE);
 		assertEquals("http:%2F%2Fwww.leveluplunch.com%3Fsomevar=abc123&someothervar", urlEscaped);
-		
 	}
 	
 	@Test
@@ -77,7 +70,4 @@ public class EscapeUrl {
 		assertEquals("http%3A%2F%2Fwww.leveluplunch.com%3Fsomevar%3Dabc123%26someothervar", urlEscaped);
 	}
 	
-	
-	
-
 }
