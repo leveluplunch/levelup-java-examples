dcf05dffa4da2f5213497b9591c6602073cc6c12
Justin Musgrove <justin.musgrove@gmail.com>
2013-12-01 10:24:52 -0600

    added decode encode url base64

---
+++ src/test/java/com/levelup/java/util/DecodeURLBase64.java
@@ -0,0 +1,56 @@
+package com.levelup.java.util;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.UnsupportedEncodingException;
+
+import org.apache.commons.codec.binary.Base64;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import com.google.common.io.BaseEncoding;
+
+/**
+ * This java example will demonstrate decoding a base64 url.
+ * 
+ * @author Justin Musgrove
+ * @see <a href='http://www.leveluplunch.com/java/examples/decode-url-to-base64/'>Decode URL Base64</a>
+ */
+public class DecodeURLBase64 {
+
+	// TODO: http://download.java.net/jdk8/docs/api/java/util/Base64.html
+	@Ignore("Java 8 not yet released")
+	@Test
+	public void string_base64_decode_java_8() {
+	}
+
+	@Test
+	public void string_base64_decode_guava() throws UnsupportedEncodingException {
+
+		String encodedURL = "aHR0cDovL2xldmVsdXBsdW5jaC5jb20vZXhhbXBsZXMvP3Bhcm09VGhpcyBwYXJhbWV0ZXI=";
+
+		byte[] decodedURLAsBytes = BaseEncoding.base64Url().decode(
+				encodedURL);
+		
+		String decodedURL = new String(decodedURLAsBytes, "utf-8");
+
+		assertEquals(
+				"http://leveluplunch.com/examples/?parm=This parameter",
+				decodedURL);
+	}
+
+	@Test
+	public void string_base64_decode_apache() throws UnsupportedEncodingException {
+
+		String encodedURL = "aHR0cDovL2xldmVsdXBsdW5jaC5jb20vZXhhbXBsZXMvP3Bhcm09VGhpcyBwYXJhbWV0ZXI=";
+
+		byte[] decodedURLAsBytes = Base64.decodeBase64(encodedURL);
+		
+		String decodedURL = new String(decodedURLAsBytes, "utf-8");
+
+		assertEquals(
+				"http://leveluplunch.com/examples/?parm=This parameter",
+				decodedURL);
+	}
+	
+}
---
+++ src/test/java/com/levelup/java/util/EncodeURLBase64.java
@@ -0,0 +1,52 @@
+package com.levelup.java.util;
+
+import static org.junit.Assert.assertEquals;
+
+import org.apache.commons.codec.binary.Base64;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import com.google.common.io.BaseEncoding;
+
+/**
+ * This java example will demonstrate encoding 
+ * url w/ Base64.
+ * 
+ * @author Justin Musgrove
+ * @see <a href='http://www.leveluplunch.com/java/examples/encode-url-to-base64/'>Encode url base64</a>
+ */
+public class EncodeURLBase64 {
+
+	// TODO: http://download.java.net/jdk8/docs/api/java/util/Base64.html
+	@Ignore("Java 8 not yet released")
+	@Test
+	public void string_base64_encode_java_8() {
+	}
+
+	@Test
+	public void string_base64_encode_guava() {
+
+		String levelUpLunchURL = "http://leveluplunch.com/examples/?parm=This parameter";
+
+		String encodedURL = BaseEncoding.base64Url().encode(
+				levelUpLunchURL.getBytes());
+
+		assertEquals(
+				"aHR0cDovL2xldmVsdXBsdW5jaC5jb20vZXhhbXBsZXMvP3Bhcm09VGhpcyBwYXJhbWV0ZXI=",
+				encodedURL);
+	}
+
+	@Test
+	public void string_base64_encoding_apache() {
+
+		String levelUpLunchURL = "http://leveluplunch.com/examples/?parm=This parameter";
+
+		String encodedURL = Base64.encodeBase64URLSafeString(levelUpLunchURL
+				.getBytes());
+
+		assertEquals(
+				"aHR0cDovL2xldmVsdXBsdW5jaC5jb20vZXhhbXBsZXMvP3Bhcm09VGhpcyBwYXJhbWV0ZXI",
+				encodedURL);
+	}
+
+}
