c5a2a862c09d4644cb2fca82dc98042c9dc9d69b
Justin Musgrove <justin.musgrove@gmail.com>
2014-01-28 19:27:13 -0600

    added iterate over for loop

---
+++ src/test/java/com/levelup/java/IterateForLoop.java
@@ -0,0 +1,53 @@
+package com.levelup.java;
+
+import java.util.Arrays;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * This java example will demonstrate how to loop over 
+ * a for loop.
+ * 
+ * @author Justin Musgrove
+ * @see <a href='http://www.leveluplunch.com//'></a>
+ */
+public class IterateForLoop {
+
+	double[] numbers = {55, 29, 68, 21, 48, 36,	92,	56,	88,	96};
+	
+	@Test
+	public void loop_for_java_1_4 () {
+		
+		for (int x = 0; x < numbers.length; x ++) {
+			System.out.println(numbers[x]);
+		}
+	}
+
+	@Test
+	public void loop_for_java_1_5 () {
+		
+		for (double number : numbers) {
+			System.out.println(number);
+		}
+	}
+
+	@Test
+	public void loop_for_java_1_8 () {
+		
+		Arrays.stream(numbers).forEach(x -> System.out.println(x));
+		
+	}
+	
+	@Ignore
+	@Test
+	public void loop_for_infinite () {
+		
+		for ( ; ; ) {
+			System.out.println("don't run this code");
+		}
+	}
+	
+
+	
+}
