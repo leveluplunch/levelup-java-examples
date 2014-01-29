a57097e846e80d2f7dadd86785e2ad01c08c98a4
Justin Musgrove <justin.musgrove@gmail.com>
2014-01-28 19:04:16 -0600

    added how to sum values in list java, java 8 and apache statutils

---
+++ src/test/java/com/levelup/java/collections/SumValueInList.java
@@ -0,0 +1,84 @@
+package com.levelup.java.collections;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.DoubleStream;
+
+import org.apache.commons.collections.ListUtils;
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.math3.stat.StatUtils;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.google.common.collect.Lists;
+import com.google.common.math.DoubleMath;
+import com.google.common.math.IntMath;
+
+/**
+ * This java example will demonstrate how to 
+ * sum values in a list.
+ * 
+ * @author Justin Musgrove
+ * @see <a href='http://www.leveluplunch.com/java/examples/sum-values-in-arraylist/'>Sum values in list</a>
+ */
+public class SumValueInList {
+
+	private static List<Double> NUMBERS_FOR_SUM = new ArrayList<Double>();
+	
+	@Before
+	public void setup () {
+		NUMBERS_FOR_SUM.add(new Double(10));
+		NUMBERS_FOR_SUM.add(new Double(10));
+		NUMBERS_FOR_SUM.add(new Double(10));
+		NUMBERS_FOR_SUM.add(new Double(10));
+		NUMBERS_FOR_SUM.add(new Double(10));
+		NUMBERS_FOR_SUM.add(new Double(10));
+		NUMBERS_FOR_SUM.add(new Double(10));
+		NUMBERS_FOR_SUM.add(new Double(10));
+		NUMBERS_FOR_SUM.add(new Double(10));
+		NUMBERS_FOR_SUM.add(new Double(10));
+	}
+
+	
+	@Test
+	public void sum_values_in_list_java () {
+		
+	     Double sum = new Double(0); 
+	     for (Double i : NUMBERS_FOR_SUM) {
+	    	 sum = sum + i;
+	     }
+	         
+		assertEquals(100, sum, 0);
+	}
+	
+	@Test
+	public void sum_values_in_list_java_8() {
+
+		double sum = NUMBERS_FOR_SUM.stream().reduce(0d, (a, b) -> a + b);
+
+		assertEquals(100, sum, 0);
+
+		// or
+
+		double sum2 = NUMBERS_FOR_SUM
+				.stream()
+				.mapToDouble(Double::doubleValue)
+				.sum();
+
+		assertEquals(100, sum2, 0);
+	}
+
+	@Test
+	public void sum_values_in_list_apache() {
+
+		double[] arrayToSume = ArrayUtils.toPrimitive(NUMBERS_FOR_SUM
+				.toArray(new Double[NUMBERS_FOR_SUM.size()]));
+
+		double sum = StatUtils.sum(arrayToSume);
+
+		assertEquals(100, sum, 0);
+	}
+	
+}
