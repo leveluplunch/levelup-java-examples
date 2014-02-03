fc78e4af684e663223eb0b889a60af64efe01f65
Justin Musgrove <justin.musgrove@gmail.com>
2014-02-02 20:43:21 -0600

    updated generate random number using Java 8 date time API

--- src/test/java/com/levelup/java/date/GenerateRandomDate.java
+++ src/test/java/com/levelup/java/date/GenerateRandomDate.java
@@ -7,6 +7,10 @@ import static org.junit.Assert.assertThat;
 
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 import org.apache.log4j.Logger;
@@ -64,6 +68,25 @@ public class GenerateRandomDate {
 		}
 	}
 
+	@Test
+	public void generate_random_date_java8() {
+
+	    DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss");
+		
+		for (int x = 0; x < 7; x++) {
+			
+			Instant instant = Instant.ofEpochSecond(getRandomTimeBetweenTwoDates());
+			LocalDateTime randomDate = LocalDateTime.ofInstant(instant, ZoneId.of("UTC-06:00")); 
+
+			logger.info(randomDate.format(format));
+
+			assertThat(
+					randomDate.atZone(ZoneId.of("UTC-06:00")).toEpochSecond(),
+					allOf(lessThanOrEqualTo(endTime),
+							greaterThanOrEqualTo(beginTime)));
+		}
+	}
+	
 	@Test
 	public void generate_random_date_joda () {
 
