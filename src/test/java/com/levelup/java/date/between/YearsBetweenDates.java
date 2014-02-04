8b4d7855f2fbdc8128304d2bc4f222d45e810f3f
Justin Musgrove <justin.musgrove@gmail.com>
2014-02-03 21:10:07 -0600

    updated  years between between dates using Java 8 date time API

--- src/test/java/com/levelup/java/date/between/YearsBetweenDates.java
+++ src/test/java/com/levelup/java/date/between/YearsBetweenDates.java
@@ -2,6 +2,11 @@ package com.levelup.java.date.between;
 
 import static org.junit.Assert.assertEquals;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.Period;
+import java.time.temporal.ChronoUnit;
+
 import org.joda.time.DateTime;
 import org.joda.time.Years;
 import org.junit.Test;
@@ -16,6 +21,21 @@ import org.junit.Test;
  */
 public class YearsBetweenDates {
 
+	@Test
+	public void years_between_two_dates_in_java_with_java8 () {
+		
+		LocalDate startDate = LocalDate.of(2004, Month.DECEMBER, 25);
+		LocalDate endDate = LocalDate.of(2006, Month.JANUARY, 1);
+
+		long numberOfYears = ChronoUnit.YEARS.between(startDate, endDate);
+		assertEquals(1, numberOfYears);
+		
+		// or 
+		
+		long numberOfYears2 = Period.between(startDate, endDate).getYears();
+		assertEquals(1, numberOfYears2);
+	}
+	
 	@Test
 	public void years_between_two_dates_in_java_with_joda () {
 		
