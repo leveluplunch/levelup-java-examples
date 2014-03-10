32736f3a6282f5f1e21d3df10729a42cd0ecd13b
Justin Musgrove <justin.musgrove@gmail.com>
2014-03-10 17:16:51 -0500

    formatting

--- src/test/java/com/levelup/java/date/query/DaylightSavingQuery.java
+++ src/test/java/com/levelup/java/date/query/DaylightSavingQuery.java
@@ -12,62 +12,63 @@ import java.time.temporal.TemporalQuery;
 import org.junit.Test;
 
 /**
- * This java example will demonstrate determining if
- * the date is within daylight saving while using TemporalQuery.
+ * This java example will demonstrate determining if the date is within daylight
+ * saving while using TemporalQuery.
  * 
  * @author Justin Musgrove
- * @see <a href='http://www.leveluplunch.com/java/examples/daylight-saving-query-java8/'>Daylight saving query</a>
+ * @see <a href=
+ *      'http://www.leveluplunch.com/java/examples/daylight-saving-query-java8/'>Daylight
+ *      saving query</a>
  */
 public class DaylightSavingQuery implements TemporalQuery<Boolean> {
 
-	/* (non-Javadoc)
-	 * @see java.time.temporal.TemporalQuery#queryFrom(java.time.temporal.TemporalAccessor)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.time.temporal.TemporalQuery#queryFrom(java.time.temporal.
+	 * TemporalAccessor)
 	 */
 	@Override
 	public Boolean queryFrom(TemporalAccessor temporal) {
-	
-		LocalDate date = LocalDate.from(temporal);
-		
-		LocalDate secondSundayInMarch = LocalDate
-			.from(date)
-			.withMonth(Month.MARCH.getValue())
-			.with(TemporalAdjusters.firstInMonth(DayOfWeek.SUNDAY))
-			.with(TemporalAdjusters.next(DayOfWeek.SUNDAY));
-		
-		LocalDate firstSundayInNovember = LocalDate
-			.from(date)
-			.withMonth(Month.NOVEMBER.getValue())
-			.with(TemporalAdjusters.firstDayOfMonth())
-			.with(TemporalAdjusters.next(DayOfWeek.SUNDAY));
 
-		if (date.isAfter(secondSundayInMarch) ||
-				date.isBefore(firstSundayInNovember)) {
+		LocalDate date = LocalDate.from(temporal);
+
+		LocalDate secondSundayInMarch = LocalDate.from(date)
+				.withMonth(Month.MARCH.getValue())
+				.with(TemporalAdjusters.firstInMonth(DayOfWeek.SUNDAY))
+				.with(TemporalAdjusters.next(DayOfWeek.SUNDAY));
+
+		LocalDate firstSundayInNovember = LocalDate.from(date)
+				.withMonth(Month.NOVEMBER.getValue())
+				.with(TemporalAdjusters.firstDayOfMonth())
+				.with(TemporalAdjusters.next(DayOfWeek.SUNDAY));
+
+		if (date.isAfter(secondSundayInMarch)
+				|| date.isBefore(firstSundayInNovember)) {
 			return true;
 		} else {
 			return false;
 		}
 	}
-	
+
 	@Test
-	public void during_daylight_savings ()  {
-		
-		LocalDate date = LocalDate.of(2014, Month.JULY, 02); 
+	public void during_daylight_savings() {
+
+		LocalDate date = LocalDate.of(2014, Month.JULY, 02);
 
 		Boolean daylightSavings = date.query(new DaylightSavingQuery());
 
 		assertTrue(daylightSavings);
 	}
-	
+
 	@Test
-	public void not_during_daylight_savings ()  {
-		
-		LocalDate date = LocalDate.of(2014, Month.DECEMBER, 02); 
+	public void not_during_daylight_savings() {
+
+		LocalDate date = LocalDate.of(2014, Month.DECEMBER, 02);
 
 		Boolean daylightSavings = date.query(new DaylightSavingQuery());
 
 		assertTrue(daylightSavings);
 	}
-	
-	
 
 }
