a0d352d32dbb77dba92c1733619d1712c1867c51
Justin Musgrove <justin.musgrove@gmail.com>
2015-02-11 21:04:49 -0600

    fix unit test

--- src/test/java/com/levelup/java/util/stream/SumUniqueValsTwoIntStreams.java
+++ src/test/java/com/levelup/java/util/stream/SumUniqueValsTwoIntStreams.java
@@ -13,8 +13,8 @@ import org.junit.Test;
  * 
  * @author Justin Musgrove
  * @see <a href=
- *      'http://www.leveluplunch.com/java/examples/sum-unique-values-two-intstream/'>Sum
- *      unique values of numeric streams</a>
+ *      'http://www.leveluplunch.com/java/examples/sum-unique-values-two-intstream/'>S
+ *      u m unique values of numeric streams</a>
  */
 public class SumUniqueValsTwoIntStreams {
 
@@ -29,8 +29,11 @@ public class SumUniqueValsTwoIntStreams {
 		assertEquals(6, val);
 
 		// or
-		OptionalInt sum2 = IntStream.concat(stream1, stream2).reduce(
-				(a, b) -> a + b);
+		IntStream stream3 = IntStream.of(1, 2, 3);
+		IntStream stream4 = IntStream.of(1, 2, 3);
+
+		OptionalInt sum2 = IntStream.concat(stream3, stream4).distinct()
+				.reduce((a, b) -> a + b);
 
 		assertEquals(6, sum2.getAsInt());
 	}
