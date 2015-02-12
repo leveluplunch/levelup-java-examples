d4e491f63fac36e5cd5b7898d1bc40e171bbefb0
Justin Musgrove <justin.musgrove@gmail.com>
2015-02-11 21:35:16 -0600

    fix unit test

--- src/test/java/com/levelup/java/xml/XMLToMapXstream.java
+++ src/test/java/com/levelup/java/xml/XMLToMapXstream.java
@@ -17,14 +17,16 @@ import com.thoughtworks.xstream.XStream;
  * This java example will demonstrate converting xml to map with xstream.
  * 
  * @author Justin Musgrove
- * @see <a href='http://www.leveluplunch.com/java/examples/convert-xml-to-from-hashmap-object-xstream/'>Convert xml to hashmap using xstream</a>
+ * @see <a href=
+ *      'http://www.leveluplunch.com/java/examples/convert-xml-to-from-hashmap-object-xstream/'>Conver
+ *      t xml to hashmap using xstream</a>
  */
 public class XMLToMapXstream {
 
 	private static final Logger logger = Logger
 			.getLogger(XMLToMapXstream.class);
 
-	class Restaurant {
+	public class Restaurant {
 
 		private Integer id;
 		private String name;
@@ -58,7 +60,8 @@ public class XMLToMapXstream {
 		Restaurant resturant = new Restaurant(1, "Woodlawn super club",
 				"Fort Atkinson");
 		Restaurant resturant2 = new Restaurant(2, "Sammy's", "Fort Atkinson");
-		Restaurant resturant3 = new Restaurant(3, "ColdSpring Inn", "Cold Spring");
+		Restaurant resturant3 = new Restaurant(3, "ColdSpring Inn",
+				"Cold Spring");
 
 		restaurantByKey = Maps.newHashMap();
 		restaurantByKey.put(1, resturant);
@@ -82,7 +85,7 @@ public class XMLToMapXstream {
 	@Test
 	public void deserialize_xml_to_hashmap() {
 
-		String xmlAsMap = "<map> <entry> <int>1</int> <com.levelup.java.xml.XMLToMapXstream_-Resturant> <id>1</id> <name>Woodlawn super club</name> <address>Fort Atkinson</address> <outer-class> <resturantByKey reference=\"../../../..\"/> </outer-class> </com.levelup.java.xml.XMLToMapXstream_-Resturant> </entry> <entry> <int>2</int> <com.levelup.java.xml.XMLToMapXstream_-Resturant> <id>2</id> <name>Sammy&apos;s</name> <address>Fort Atkinson</address> <outer-class reference=\"../../../entry/com.levelup.java.xml.XMLToMapXstream_-Resturant/outer-class\"/> </com.levelup.java.xml.XMLToMapXstream_-Resturant> </entry> <entry> <int>3</int> <com.levelup.java.xml.XMLToMapXstream_-Resturant> <id>2</id> <name>ColdSpring Inn</name> <address>Cold Spring</address> <outer-class reference=\"../../../entry/com.levelup.java.xml.XMLToMapXstream_-Resturant/outer-class\"/> </com.levelup.java.xml.XMLToMapXstream_-Resturant> </entry> </map>";
+		String xmlAsMap = "<map>\n <entry>\n <int>1</int>\n <com.levelup.java.xml.XMLToMapXstream_-Restaurant>\n <id>1</id>\n <name>Woodlawn super club</name>\n <address>Fort Atkinson</address>\n <outer-class>\n <restaurantByKey reference=\"../../../..\"/>\n </outer-class>\n </com.levelup.java.xml.XMLToMapXstream_-Restaurant>\n </entry>\n <entry>\n <int>2</int>\n <com.levelup.java.xml.XMLToMapXstream_-Restaurant>\n <id>2</id>\n <name>Sammy&apos;s</name>\n <address>Fort Atkinson</address>\n <outer-class reference=\"../../../entry/com.levelup.java.xml.XMLToMapXstream_-Restaurant/outer-class\"/>\n </com.levelup.java.xml.XMLToMapXstream_-Restaurant>\n </entry>\n <entry>\n <int>3</int>\n <com.levelup.java.xml.XMLToMapXstream_-Restaurant>\n <id>3</id>\n <name>ColdSpring Inn</name>\n <address>Cold Spring</address>\n <outer-class reference=\"../../../entry/com.levelup.java.xml.XMLToMapXstream_-Restaurant/outer-class\"/>\n </com.levelup.java.xml.XMLToMapXstream_-Restaurant>\n </entry>\n</map>\n";
 
 		XStream xStream = new XStream();
 		xStream.alias("map", java.util.Map.class);
